14692179cb870d5c1b3c68b427ce794c21d75942
Rusiru Salwathura <rusirusalwathura@icloud.com>
2026-07-14 18:50:31 +0530

    fix(hero): fix typo in Hero CTA button label

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -91,7 +91,7 @@ export default function Hero() {
           <div className="flex select-none">
             <Link href="#featuredSection">
               <Button variant="cta" size="cta">
-                Contact U
+                Contact Us
               </Button>
             </Link>
           </div>
